b9b8f31310e600f286e62e2f7410f636e89b1442
hslee <hanseul.dev@gmail.com>
2025-02-04 21:52:42 +0900

    feat: 초기 폴더 구조 정리 및 styled-components 적용

---
+++ frontend/src/components/common/Header.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import * as S from "./Header.style";
+
+export default function Header() {
+  return (
+    <div>
+      <header className="header">
+        <S.Title>오이</S.Title>
+        <div className="logo">
+          <img src="../assets/images/logo.png" alt="로고" />
+        </div>
+        <nav className="nav">
+          <a href="/buy" className="nav-button">
+            중고거래
+          </a>
+          <a href="/mypage" className="mypage-button">
+            마이페이지
+          </a>
+        </nav>
+      </header>
+    </div>
+  );
+}
