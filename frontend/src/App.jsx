2e863ac1f07677896c9460ce9e4c979c713ff292
Codename_Chichi <nochecita2003@gmail.com>
2025-02-09 18:48:33 +0900

    feat: Route 추가

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,10 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Main, Login } from "./components";
 import { createGlobalStyle } from "styled-components";
 import MyPage from "./components/mypage/MyPage";
+import MyInfo from "./components/mypage/MyInfo";
+import MyPosts from "./components/mypage/MyPosts";
+import LikePosts from "./components/mypage/LikePosts";
+import MyComments from "./components/mypage/MyComments";
 
 const GlobalStyle = createGlobalStyle`
   body {
@@ -20,6 +24,10 @@ function App() {
           <Route path="/" element={<Login />} />
           <Route path="/main" element={<Main />} />
           <Route path="/mypage" element={<MyPage />} />
+          <Route path="/mypage/myinfo" element={<MyInfo />} />
+          <Route path="/mypage/myposts" element={<MyPosts />} />
+          <Route path="/mypage/likeposts" element={<LikePosts />} />
+          <Route path="/mypage/mycomments" element={<MyComments />} />
         </Routes>
       </BrowserRouter>
     </>
