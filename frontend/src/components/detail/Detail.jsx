faac8e8cc5d1f2db175d11802f60d9a15ae6929b
Codename_Chichi <nochecita2003@gmail.com>
2025-02-13 01:04:00 +0900

    style: 상세 페이지 스타일 추가

--- frontend/src/components/detail/Detail.jsx
+++ frontend/src/components/detail/Detail.jsx
@@ -1,3 +1,17 @@
-export function Detail() {
-  return <div>Detail 페이지</div>;
+import React, { useState } from "react";
+import * as S from "./MyInfo.style";
+import Header from "../common/Header";
+import { useNavigate } from "react-router-dom";
+
+const User = {
+  id: "ewha1886",
+  pw: "womansuni012!"
+};
+
+export default function MyInfo() {
+  return (
+    <div>
+      <Header />
+    </div>
+  );
 }
