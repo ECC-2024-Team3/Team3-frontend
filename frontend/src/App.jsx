5cf5ee857feb22a99a00f0972b109e64b919ca3a
Codename_Chichi <nochecita2003@gmail.com>
2025-02-14 18:37:13 +0900

    feat: 상품 상세 페이지 기능 추가

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,6 +9,7 @@ import {
   MyPosts,
   MyInfo,
   LikePosts,
+  Detail,
 } from "./components";
 import { createGlobalStyle } from "styled-components";
 
@@ -36,6 +37,7 @@ function App() {
         <Route path="/mypage/myposts" element={<MyPosts />} />
         <Route path="/mypage/likeposts" element={<LikePosts />} />
         <Route path="/mypage/mycomments" element={<MyComments />} />
+        <Route path="/post/:postId" element={<Detail />} />
       </Routes>
     </BrowserRouter>
   );
--- frontend/src/components/detail/Detail.jsx
+++ frontend/src/components/detail/Detail.jsx
@@ -1,17 +1,70 @@
 import React, { useState } from "react";
 import * as S from "./Detail.style";
 import Header from "../common/Header";
-import { useNavigate } from "react-router-dom";
+import { useParams } from "react-router-dom";
+import likedbuttonfillpng from "./liked_button_fill.png"
+import likedbuttonemptypng from "./liked_button_empty.png"
 
 const User = {
   id: "ewha1886",
-  pw: "womansuni012!"
+  pw: "womansuni012!",
+  name: "이대댕김",
 };
 
-export default function Detail() {
+export function Detail() {
+
+  const { productId } = useParams();
+  const [product, setProduct] = useState(null);
+
   return (
     <div>
       <Header />
+
+      <S.TitleContainer>
+        <S.MainTitle>중고 거래 상세 페이지</S.MainTitle>
+        <S.LikeButton>
+          {product.liked ? {likedbuttonfillpng} : {likedbuttonemptypng}}
+        </S.LikeButton>
+        <S.BookmarkButton>
+          {product.bookmarked ? "" : ""}
+        </S.BookmarkButton>
+      </S.TitleContainer>
+      <S.Line />
+
+      <S.ProductContainer>
+        <S.ProductImage src={product.images[0]} alt="상품 이미지" />
+
+      <S.ProductDetails>
+      <S.ProductTitle>{product.title}</S.ProductTitle>
+      <S.InfoRow>
+        <S.InfoLabel>카테고리</S.InfoLabel>
+        <S.InfoValue>전자기기</S.InfoValue>
+      </S.InfoRow>
+      <S.InfoRow>
+        <S.InfoLabel>상품 상태</S.InfoLabel>
+        <S.InfoValue>{product.transaction_status}</S.InfoValue>
+      </S.InfoRow>
+      <S.InfoRow>
+        <S.InfoLabel>거래 장소</S.InfoLabel>
+        <S.InfoValue>{product.location}</S.InfoValue>
+      </S.InfoRow>
+      <S.InfoRow>
+        <S.InfoLabel>가격</S.InfoLabel>
+        <S.InfoValue>{product.price.toLocaleString()}원</S.InfoValue>
+      </S.InfoRow>
+      </S.ProductDetails>
+      </S.ProductContainer>
+
+      <S.Description>
+        <S.InfoLabel>설명</S.InfoLabel>
+        <S.DescText>{product.content}</S.DescText>
+      </S.Description>
+
+      <S.SellerInfo>
+        <S.InfoLabel>거래자 정보</S.InfoLabel>
+        <S.SellerName>판매자 ID: {product.user_id}</S.SellerName>
+      </S.SellerInfo>
+
     </div>
   );
 }
