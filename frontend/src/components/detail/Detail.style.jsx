423a708f5c7f721bd1d5a3046d98cfabbd1b725a
Codename_Chichi <nochecita2003@gmail.com>
2025-02-15 18:01:33 +0900

    style: 상품 상세 페이지 스타일 수정

--- frontend/src/components/detail/Detail.style.jsx
+++ frontend/src/components/detail/Detail.style.jsx
@@ -1,56 +1,107 @@
 import styled from "styled-components";
 
-export const Container = styled.div`
-`;
 export const TitleContainer = styled.div`
-`;
-
-export const MainTitle = styled.h1`
-`;
-
-export const LikeButton = styled.button`
-`;
-export const Line = styled.hr`
-`;
-
-export const ProductContainer = styled.div`
-`;
-
-export const ProductImage = styled.img`
-`;
-
-export const ProductDetails = styled.div`
-`;
-
-export const ProductTitle = styled.h2`
-`;
-
-export const InfoRow = styled.div`
-`;
-
-export const InfoLabel = styled.div`
-`;
-
-export const InfoValue = styled.div`
-`;
-
-export const Description = styled.div`
-`;
-
-export const DescText = styled.p`
-`;
-
-export const SellerInfo = styled.div`
-`;
-
-export const SellerName = styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 0 20px 0 20px;
 `;
 
 export const ButtonContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+export const MainTitle = styled.h1`
+    font-size: 32px;
+    color: #00462a;
+`;
+
+export const Button = styled.button`
+    background: none;
+    border: none;
+    cursor: pointer;
+`;
+
+export const Line = styled.hr`
+    border: none;
+    border-top: 3px solid #00462a;
+    margin-top: -10px;
+    margin-bottom: 20px;
+    width: 95%;
+`;
+
+export const ProductContainer = styled.div`
+  display: flex;
+  gap: 20px;
+  margin-bottom: 20px;
+  padding: 0 20px 0 20px;
+`;
+
+export const ProductImage = styled.img`
+  width: 250px;
+  height: 250px;
+  object-fit: cover;
+  border-radius: 10px;
+`;
+
+export const ProductDetails = styled.div`
+    flex: 1;
+    margin-top: -20px;
+`;
+
+export const ProductTitle = styled.h2`
+  font-size: 32px;
+  margin-bottom: 10px;
+`;
+
+export const InfoRow = styled.div`
+  font-size: 24px;
+  display: flex;
+  margin-bottom: 10px;
+`;
+
+export const InfoLabel = styled.div`
+  width: 100px;
+  font-weight: bold;
+  color: #00462a;
+`;
+
+export const InfoValue = styled.div`
+  flex: 1;
+`;
+
+export const PriceValue = styled.div`
+  font-size: 48px;
+`;
+
+export const Description = styled.div`
+  border: 1px solid #ddd;
+  padding: 20px 20px;
+  margin: 20px;
+`;
+
+export const DescText = styled.p`
+  margin-top: 10px;
+  color: #333;
+`;
+
+export const SellerInfo = styled.div`
+  border: 1px solid #ddd;
+  padding: 20px 20px;
+  margin: 20px;
+`;
+
+export const SellerName = styled.div`
+  margin-top: 10px;
+  color: #333;
 `;
 
 export const ChatButton = styled.button`
-`;
-
-export const BookmarkButton = styled.button`
+  padding: 10px 20px;
+  background-color: #00462a;
+  color: white;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
 `;
