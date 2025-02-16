7a8aa51caf2be99764f075d1a99ce83577a1fc4f
Codename_Chichi <nochecita2003@gmail.com>
2025-02-17 02:04:03 +0900

    feat: 댓글 기능 페이지 추가

---
+++ frontend/src/components/comment/Comments.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import * as S from "./Comments.style";
+import Header from "../common/Header";
+import { Link } from "react-router-dom";
+
+export function Comments() {
+  return (
+    <div>
+      <Header />
+    </div>
+  );
+}
