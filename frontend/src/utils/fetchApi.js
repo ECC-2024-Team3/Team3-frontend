6a1f2cac001f1b5691a09a5e88fba09391c6a7f3
Codename_Chichi <nochecita2003@gmail.com>
2025-02-26 03:02:11 +0900

    fix: 댓글 로딩 오류 수정

--- frontend/src/utils/fetchApi.js
+++ frontend/src/utils/fetchApi.js
@@ -1,15 +1,24 @@
 export const fetchApi = async (url, options) => {
   try {
+    const token = localStorage.getItem("token");
     const response = await fetch(url, {
       headers: {
         // TODO: 추후 토큰 추가
-        // Authorization: `Bearer ${token}`,
+        Authorization: `Bearer ${token}`,
         "Content-Type": "application/json",
       },
       ...options,
     });
-    const data = await response.json();
-    return data;
+
+    const contentType = response.headers.get("Content-Type");
+
+    if (contentType && contentType.includes("application/json")) {
+      const data = await response.json();
+      return data;
+    } else {
+      const textData = await response.text();
+      return textData;
+    }
   } catch (error) {
     console.error("Error:", error);
   }
