35d591f110057abe3151cb5eb8165f906a739a81
Codename_Chichi <nochecita2003@gmail.com>
2025-02-26 23:18:52 +0900

    feat: 로그아웃 기능 추가

--- frontend/src/components/mypage/MyPage.jsx
+++ frontend/src/components/mypage/MyPage.jsx
@@ -19,10 +19,49 @@ export function MyPage() {
       });
 
       alert("회원 탈퇴가 완료되었습니다.");
+      localStorage.removeItem("token");
+      localStorage.removeItem("userId");
       navigate("/");
     } catch (err) {
       console.error(err);
       alert("탈퇴 중 오류가 발생했습니다.");
+    }
+  };
+
+  const handleLogout = async () => {
+    try {
+      const token = localStorage.getItem("token");
+      const email = localStorage.getItem("email");
+      const password = localStorage.getItem("password");
+
+      if (!token || !email || !password) {
+        alert("로그인 정보가 없습니다.");
+        return;
+      }
+
+      const response = await fetchApi(API_URLS.logout, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+        body: JSON.stringify({ email, password }),
+      });
+
+      if (response?.status === 200) {
+        alert(response.message || "로그아웃 성공");
+
+        localStorage.removeItem("token");
+        localStorage.removeItem("userId");
+        localStorage.removeItem("email");
+        localStorage.removeItem("password");
+        navigate("/");
+      } else {
+        alert(response?.message || "로그아웃 실패");
+      }
+    } catch (err) {
+      console.error("로그아웃 오류:", err);
+      alert("로그아웃 중 오류가 발생했습니다.");
     }
   };
 
@@ -40,7 +79,7 @@ export function MyPage() {
           <S.Button>내 댓글 관리</S.Button>
         </Link>
         <Link to="/" style={{ textDecorationLine: "none" }}>
-          <S.Button>로그아웃</S.Button>
+          <S.Button onClick={handleLogout}>로그아웃</S.Button>
         </Link>
         <S.Text onClick={handleDeleteUser} style={{ cursor: "pointer" }}>
           탈퇴하기
