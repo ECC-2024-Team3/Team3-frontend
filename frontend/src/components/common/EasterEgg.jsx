2c91abc384b17baf9381d498fce81d6b087757f5
Codename_Chichi <nochecita2003@gmail.com>
2025-02-27 03:42:57 +0900

    feat: 만든 사람 페이지 추가

--- frontend/src/components/common/EasterEgg.jsx
+++ frontend/src/components/common/EasterEgg.jsx
@@ -3,17 +3,15 @@ import styled from "styled-components";
 import Header from "../common/Header";
 import cucumberpng from "./cucumber.png";
 
-export default function IntroPage() {
+export function EasterEgg() {
   return (
     <>
       <Header />
       <Container>
-        <TopBanner>
           <MainTitle>이화여자대학교 교내 중고거래 플랫폼</MainTitle>
-        </TopBanner>
-        <GreenBanner>
+        <Banner>
           <CucumberIcon src={cucumberpng} alt="오이" />
-        </GreenBanner>
+        </Banner>
         <TeamInfo>
           <TeamTitle>만든 사람 : ECC 프로젝트 3팀</TeamTitle>
           <RoleText>UX/UI 이서연 최다빈</RoleText>
@@ -30,48 +28,45 @@ const Container = styled.div`
   flex-direction: column;
   align-items: center;
   width: 100%;
-`;
-
-const TopBanner = styled.div`
-  width: 100%;
-  background-color: #004225;
-  display: flex;
-  justify-content: center;
-  padding: 20px 0;
+  background-color: #00462A;
 `;
 
 const MainTitle = styled.h1`
-  color: #ffffff;
-  margin: 0;
+  margin: 20;
   font-size: 24px;
+  font-weight: 400;
+  color: white;
 `;
 
-const GreenBanner = styled.div`
+const Banner = styled.div`
   width: 100%;
-  background-color: #1e8c48;
   display: flex;
   justify-content: center;
   align-items: center;
   padding: 15px 0;
+  background-color: white;
 `;
 
 const CucumberIcon = styled.img`
-  width: 30px;
+  width: 40%;
   height: auto;
   margin-right: 8px;
 `;
 
 const TeamInfo = styled.div`
-  margin-top: 40px;
+  margin-bottom: 20px;
   text-align: center;
 `;
 
 const TeamTitle = styled.h3`
-  font-size: 20px;
+  font-size: 42px;
   margin-bottom: 16px;
+  color: white;
+  font-weight: 400;
 `;
 
 const RoleText = styled.div`
-  font-size: 16px;
-  margin: 4px 0;
+  font-size: 36px;
+  margin: 4px;
+  color: white;
 `;
